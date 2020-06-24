834981e7093f2ff901e2069fc7ca66446297d479
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-06-24 15:41:14 +1000

    database updated

--- app/controllers/burger_controller.rb
+++ app/controllers/burger_controller.rb
@@ -1,11 +1,45 @@
 class BurgerController < ApplicationController  
 
- def login 
+ def login  
+  
+ end  
 
- end 
+ def processing   
+   
+   
+ end  
+
+
+#sign up methods
+
+ def signup  
+     
+ end  
+
+
+
+ def  signup_data 
+   
+   if session[:new_profile] == nil   
+      session[:new_profile] = [] 
+   end  
+
+   session[:new_profile].push(params[:profile]) 
+
+   @profile = session[:new_profile]   
+   puts "_____________________________"
+   puts @profile.last["username"] 
+   
+   redirect_to profile_path(@profile.last["username"]) 
+ end
 
  def profile 
+    puts "----------------"  
+    puts params 
 
+    @profile = session[:new_profile] 
+    puts @profile 
+    
  end 
 
  def match 
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,29 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html 
 
   
-  root "burger#login"  
+  root "burger#login", as: "login" 
+  
+  # post "/sign_up", to: "burger#sign_up"
 
-  post "/", to: "burger#login" 
+  post "/", to: "burger#processing"  
 
-  get "/profile", to: "burger#profile", as: "profile"   
+  
+  # sign up routes
 
-  post "/profile", to: "burger#profile" 
+  get "/signup", to: "burger#signup", as: "signup" 
+
+  post "/signup", to: "burger#signup_data", as: "signup_data"
+
+
+  # profile routes
+
+  get "/profile/:name", to: "burger#profile", as: "profile"   
+
+  post "/profile/:name", to: "burger#profile"  
+
+
+
+  # match routes
 
   get "/match", to: "burger#match", as: "match"  
 
